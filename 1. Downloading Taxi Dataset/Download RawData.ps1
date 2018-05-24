717a3fc3e96bd6e9aa1b89c975a8afd2eabc0524
pansaty <pansaty@microsoft.com>
2018-05-23 23:36:26 -0400

    Adding files to download taxi dataset

---
+++ 1. Downloading Taxi Dataset/Download RawData.ps1
@@ -0,0 +1,8 @@
+﻿$urls = Get-Content -Path "<PATH>\raw_uber_data_urls.txt"
+$destfolder = "<PATH>\Azure\Azure SQLDW\NYC Data\Uber\"
+foreach ($url in $urls)
+{
+    $dest = $url -split "data/"
+    $output =$destfolder+$dest[1]
+    Start-BitsTransfer -Source $url -Destination $output
+}
